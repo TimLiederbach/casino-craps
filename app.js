b1b74bc66d9337bbdf77531c5347f28887030aac
tim liederbach <timmy@ool-ad020205.dyn.optonline.net>
2018-03-30 15:50:58 -0400

    game logic 1.0

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //eslint-env jquery
 $(document).ready(function() {
 
-let pointNumberValue = null;
+let pointNumberValue = undefined;
 
 let pointEstablished = false;
+
+let liveBets = true;
 
 const chipsHighlight = $('.chip').on('mouseover', function () {
   $(this).addClass('highlight')
@@ -43,23 +45,31 @@ const passBet = $('.pass').on('click', function () {
   $(this).children().removeClass('selected');
   });
 
+const dontPassBet = $('.dont-pass').on('click', function () {
+  console.log("selected");
+  $( ".selected" ).clone().appendTo(this);
+  $(this).children().removeClass('highlight');
+  $(this).children().removeClass('selected');
+  });
+
+let diceTotal = undefined;
+
 function rollDice () {
+  if (liveBets === true) {
   let d1 = Math.floor(Math.random() * 6) + 1;
   let d2 = Math.floor(Math.random() * 6) + 1;
-  let diceTotal = d1 + d2;
+  diceTotal = d1 + d2;
   $(".die1").text(`${d1}`);
   $(".die2").text(`${d2}`);
   $("h3").text(`You rolled a ${diceTotal}.`)
+  gamePlay ();
+  }  else {
+    alert('Please place a bet before you roll!');
+  }
 }
-  // let die1 = $('.die1');
-  // let die2 = $('.die2');
-  // let outcome = $('.outcome');
-
 
 const clickDice = $('button').on('click', rollDice);
 
-
-let diceValue = null;
 
 const playerName = $('#form input[name=username]').val();
 
@@ -69,21 +79,35 @@ class Player {
   constructor(name, balance) {
     this.name = name;
     this.balance = balance
+    this.pass = 20;
+    this.dontPass = 0;
+  }
+  winPass () {
+    this.balance += this.pass;
+  }
+  losePass () {
+    this.pass = 0;
+  }
+  winDontPass () {
+    this.balance += this.dontPass;
+  }
+  loseDontPass () {
+    this.dontPass = 0;
   }
 }
 
-class Pass extends Player {
-  constructor(name, balance) {
-    super(name, balance);
-    this.pass = 0;
-  }
-  win () {
-    this.balance += this.pass;
-  }
-  lose () {
-    this.pass = 0;
-  }
-}
+// class Pass extends Player {
+//   constructor(name, balance) {
+//     super(name, balance);
+//     this.pass = 20;
+//   }
+//   win () {
+//     this.balance += this.pass;
+//   }
+//   lose () {
+//     this.pass = 0;
+//   }
+// }
 
 class dontPass extends Player {
   constructor(name, balance) {
@@ -139,17 +163,43 @@ class Buy extends Player {
   }
 }
 
-const readyPlayerOne = new Player(playerName, playerOneBalance);
+const readyPlayerOne = new Player('Tim', 200);
 
 const gamePlay = function() {
-  if (diceValue = 7||11) {
-    ;
+  if (pointEstablished === false) {
+    console.log('working-false');
+      if (diceTotal === 7 || diceTotal === 11) {
+        readyPlayerOne.winPass();
+        readyPlayerOne.loseDontPass();
+        console.log(readyPlayerOne.balance)
+      } else if (diceTotal === 2 || diceTotal === 3 || diceTotal === 12)  {
+        readyPlayerOne.losePass();
+        readyPlayerOne.winDontPass();
+        console.log(readyPlayerOne.balance)
+      } else {
+          alert(`The Point is ${diceTotal}!`);
+          pointNumberValue = diceTotal;
+          pointEstablished = true;
+          console.log(readyPlayerOne.balance)
+      }
+  } else if (pointEstablished === true) {
+      console.log('working-false');
+        if (diceTotal === 7)  {
+          readyPlayerOne.losePass();
+          readyPlayerOne.winDontPass();
+          pointEstablished = false;
+          console.log(readyPlayerOne.balance)
+        } else if (diceTotal === pointNumberValue) {
+          readyPlayerOne.winPass();
+          readyPlayerOne.loseDontPass();
+          pointEstablished = false;
+          console.log(readyPlayerOne.balance)
+        }
   }
 }
 
 
-console.log(pointEstablished)
-console.log(chipValue)
+
 
 
 });
